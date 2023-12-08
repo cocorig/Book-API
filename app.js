b7d30650ccbf50117e83af42b2c46eb6bd15d91f
Hayeon LEE <fhdodl@naver.com>
2023-12-09 01:53:17 +0900

    refactor: 데이터 압축

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const compression = require("compression");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -5,19 +6,20 @@ const axios = require("axios");
 const app = express();
 const port = 8080;
 require("dotenv").config();
+app.use(compression());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
 const aladinApiKey = process.env.REACT_APP_ALADIN_API_KEY;
 const naverClientId = process.env.REACT_APP_NAVER_CLIENT_ID;
 const naverClientSecret = process.env.REACT_APP_NAVER_CLIENT_SECRET;
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
 const aladinApiBaseUrl = "https://www.aladin.co.kr/ttb/api/ItemList.aspx";
 const aladinApiSearchUrl = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx";
 const aladinApiLookUpUrl = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx";
 const naverApiBaseUrl = "https://openapi.naver.com/v1/search/book.json";
 
-// 공통 함수로 API 요청을 처리하는 함수
 const fetchData = async (url, headers = {}) => {
   try {
     const response = await axios.get(url, { headers });
@@ -26,8 +28,18 @@ const fetchData = async (url, headers = {}) => {
     throw new Error("에러 발생");
   }
 };
-
-// 베스트셀러 정보 가져오기
+app.use(
+  compression({
+    level: 6,
+    threshold: 2 * 1024,
+    filter: (req, res) => {
+      if (req.headers["x-no-compression"]) {
+        return false;
+      }
+      return compression.filter(req, res);
+    },
+  })
+);
 app.get("/bestseller", async (req, res) => {
   const queryType = "Bestseller";
   const aladinApiUrl = `${aladinApiBaseUrl}?ttbkey=${aladinApiKey}&QueryType=${queryType}&MaxResults=100&start=1&SearchTarget=Book&output=js&Cover=Big&Version=20131101`;
@@ -40,8 +52,7 @@ app.get("/bestseller", async (req, res) => {
   }
 });
 
-// 신간 도서 정보 가져오기
-app.get("/newBooks", async (req, res) => {
+app.get("/newbooks", async (req, res) => {
   const queryType = "ItemNewAll";
   const aladinApiUrl = `${aladinApiBaseUrl}?ttbkey=${aladinApiKey}&QueryType=${queryType}&MaxResults=100&start=1&SearchTarget=Book&output=js&Cover=Big&Version=20131101`;
 
@@ -52,8 +63,8 @@ app.get("/newBooks", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-// 주목할 신작도서 가져오기
-app.get("/NewBookSpecial", async (req, res) => {
+
+app.get("/special", async (req, res) => {
   const queryType = "ItemNewSpecial";
   const aladinApiUrl = `${aladinApiBaseUrl}?ttbkey=${aladinApiKey}&QueryType=${queryType}&MaxResults=100&start=1&SearchTarget=Book&output=js&Cover=Big&Version=20131101`;
 
@@ -65,22 +76,17 @@ app.get("/NewBookSpecial", async (req, res) => {
   }
 });
 
-//도서 검색기능
 app.get("/search", async (req, res) => {
-  const isbn = req.query.isbn; // ISBN 정보
-  const searchQuery = req.query.searchQuery; // 사용자 검색 정보
+  const { isbn, searchQuery } = req.query;
   let naverApiUrl = "";
   let aladinApiUrl = "";
 
   if (isbn) {
-    // ISBN 정보를 이용해 네이버 API로 도서 정보 가져오기
     naverApiUrl = `${naverApiBaseUrl}?query=${isbn}`;
-    // ISBN 정보를 이용해 알라딘 API로 도서 정보 가져오기
     aladinApiUrl = `${aladinApiLookUpUrl}?ttbkey=${aladinApiKey}&itemIdType=ISBN&ItemId=${isbn}&output=js&Cover=Big&Version=20131101`;
   } else if (searchQuery) {
-    // 사용자 검색 정보를 이용해 네이버 API로 도서 정보 가져오기
     naverApiUrl = `${naverApiBaseUrl}?query=${encodeURIComponent(searchQuery)}`;
-    // 사용자 검색 정보를 이용해 알라딘 API로 도서 정보 가져오기
+
     aladinApiUrl = `${aladinApiSearchUrl}?ttbkey=${aladinApiKey}&Query=${encodeURIComponent(
       searchQuery
     )}&MaxResults=100&start=1&SearchTarget=Book&output=js&Cover=Big&Version=20131101`;
@@ -111,33 +117,6 @@ app.get("/search", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
-// app.get("/search", async (req, res) => {
-//   const isbn = req.query.isbn; // ISBN 정보
-//   const searchQuery = req.query.searchQuery; // 사용자 검색 정보
-
-//   let naverApiUrl = "";
-//   if (isbn) {
-//     // ISBN 정보를 이용해 도서 정보 가져오기
-//     naverApiUrl = `${naverApiBaseUrl}?query=${isbn}`;
-//   } else if (searchQuery) {
-//     // 사용자 검색 정보를 이용해 도서 정보 가져오기
-//     naverApiUrl = `${naverApiBaseUrl}?query=${encodeURIComponent(searchQuery)}`;
-//   } else {
-//     res.status(400).json({ error: "ISBN 정보 또는 검색어가 필요합니다." });
-//     return;
-//   }
-//   const headers = {
-//     "X-Naver-Client-Id": naverClientId,
-//     "X-Naver-Client-Secret": naverClientSecret,
-//   };
-//   try {
-//     const data = await fetchData(naverApiUrl, headers);
-//     res.json(data);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
